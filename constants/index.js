1fe262e46a0c388605291a5e3eae011372a40636
AHSAN RAZA <ahsan.raza@studentambassadors.com>
2023-10-21 17:25:33 +0500

    feat(app): add team section

--- constants/index.js
+++ constants/index.js
@@ -1,125 +1,185 @@
 export const navLinks = [
-  { href: "#about", label: "About" },
-  { href: "#schedule", label: "Event Schedule" },
-  { href: "#team", label: "Team" },
-  { href: "#partners", label: "Partners" },
-  { href: "#sponsers", label: "Sponsers" },
-  { href: "#highlights", label: "Last Year Highlights" },
-  { href: "#faq", label: "FAQs" },
+  { href: '#about', label: 'About' },
+  { href: '#schedule', label: 'Event Schedule' },
+  { href: '#team', label: 'Team' },
+  { href: '#partners', label: 'Partners' },
+  { href: '#sponsers', label: 'Sponsers' },
+  { href: '#highlights', label: 'Last Year Highlights' },
+  { href: '#faq', label: 'FAQs' },
+];
+
+export const teamMembers = [
+  {
+    name: 'Daniyal Ahmad',
+    description: 'Designation',
+    imageUrl: '/images/Ak1.png',
+  },
+  {
+    name: 'Nabeel Ahmad',
+    description: 'Designation',
+    imageUrl: '/images/pic.jpg',
+  },
+  {
+    name: 'Arslan Khalid',
+    description: 'Designation',
+    imageUrl: '/images/Ak1.png',
+  },
+  {
+    name: 'Name 4',
+    description: 'Designation',
+    imageUrl: '/images/pic.jpg',
+  },
+  {
+    name: 'Name 5',
+    description: 'Designation',
+    imageUrl: '/images/Ak1.png',
+  },
+  {
+    name: 'Name 6',
+    description: 'Designation',
+    imageUrl: '/images/pic.jpg',
+  },
+  {
+    name: 'Name 7',
+    description: 'Designation',
+    imageUrl: '/images/Ak1.png',
+  },
+  {
+    name: 'Name 8',
+    description: 'Designation',
+    imageUrl: '/images/pic.jpg',
+  },
+  {
+    name: 'Name 9',
+    description: 'Designation',
+    imageUrl: '/images/Ak1.png',
+  },
+  {
+    name: 'Name 10',
+    description: 'Designation',
+    imageUrl: '/images/pic.jpg',
+  },
+  {
+    name: 'Name 11',
+    description: 'Designation',
+    imageUrl: '/images/Ak1.png',
+  },
+
+  // Add more team members as needed
 ];
 
 export const eventSchedule = [
   {
-    eventTitle: "Open-Source Day",
-    eventDate: "19/10/2023",
+    eventTitle: 'Open-Source Day',
+    eventDate: '19/10/2023',
     eventDetails: [
       {
-        startsAt: "03:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '03:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "04:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '04:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "05:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '05:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "06:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '06:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "07:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '07:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "08:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '08:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "09:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '09:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "10:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '10:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
     ],
   },
   {
-    eventTitle: "Gaming Competition",
-    eventDate: "20/10/2023",
+    eventTitle: 'Gaming Competition',
+    eventDate: '20/10/2023',
     eventDetails: [
       {
-        startsAt: "03:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '03:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "04:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '04:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "05:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '05:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "06:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '06:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "07:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '07:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "08:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '08:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "09:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '09:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "10:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '10:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
     ],
   },
   {
-    eventTitle: "Hacking Competition",
-    eventDate: "21/10/2023",
+    eventTitle: 'Hacking Competition',
+    eventDate: '21/10/2023',
     eventDetails: [
       {
-        startsAt: "03:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '03:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "04:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '04:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "05:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '05:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "06:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '06:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "07:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '07:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "08:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '08:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "09:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '09:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
       {
-        startsAt: "10:00 PM",
-        sessionTitle: "Registration & Networking",
+        startsAt: '10:00 PM',
+        sessionTitle: 'Registration & Networking',
       },
     ],
   },
