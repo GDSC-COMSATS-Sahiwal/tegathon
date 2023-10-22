cda62de0be2ce66b093fbac728fea9640c488133
shahzaib-ai <shahzaib1234@outlook.com>
2023-10-22 13:55:27 +0500

    fixed image path issues for partners and sponsers

--- sections/Partners.jsx
+++ sections/Partners.jsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+
+import github from "../public/images/partners/github.png";
+import universe from "../public/images/partners/universe.png";
+
 const Partners = () => {
   return (
     <section
@@ -11,60 +15,25 @@ const Partners = () => {
 
       <div className="grid grid-cols-8 max-sm:grid-cols-1 w-full border border-yellow-400 rounded-lg md:rounded-3xl max-sm:border-none">
         <div className="col-start-1 col-span-3 p-4 max-sm:col-span-1 max-sm:col-start-1 flex items-center justify-center border backdrop-blur-sm border-yellow-400 rounded-tl-lg md:rounded-tl-3xl max-sm:rounded-tr-lg">
-          <Image
-            src="/images/partners/github.png"
-            height={350}
-            width={350}
-            alt="Github"
-          />
+          <Image src={github} height={350} width={350} alt="Github" />
         </div>
         <div className="col-start-4 col-span-2 p-4 max-sm:col-span-1 max-sm:col-start-1 flex items-center justify-center border backdrop-blur-sm border-yellow-400">
-          <Image
-            src="/images/partners/universe.png"
-            height={350}
-            width={350}
-            alt="Universe23"
-          />
+          <Image src={universe} height={350} width={350} alt="Universe23" />
         </div>
         <div className="col-start-6 col-span-3 max-sm:col-span-1 max-sm:col-start-1 p-4 flex items-center justify-center border backdrop-blur-sm border-yellow-400 rounded-tr-lg md:rounded-tr-3xl  max-sm:rounded-none">
-          <Image
-            src="/images/partners/universe.png"
-            height={350}
-            width={350}
-            alt="Universe23"
-          />
+          <Image src={universe} height={350} width={350} alt="Universe23" />
         </div>
         <div className="col-start-1 col-span-2 max-sm:col-span-1 max-sm:col-start-1 p-4 flex items-center justify-center border backdrop-blur-sm border-yellow-400 rounded-bl-lg md:rounded-bl-3xl  max-sm:rounded-none">
-          <Image
-            src="/images/partners/universe.png"
-            height={350}
-            width={350}
-            alt="Universe23"
-          />
+          <Image src={universe} height={350} width={350} alt="Universe23" />
         </div>
         <div className="col-start-3 col-span-2 max-sm:col-span-1 max-sm:col-start-1 p-4 flex items-center justify-center border backdrop-blur-sm border-yellow-400">
-          <Image
-            src="/images/partners/universe.png"
-            height={350}
-            width={350}
-            alt="Universe23"
-          />
+          <Image src={universe} height={350} width={350} alt="Universe23" />
         </div>
         <div className="col-start-5 col-span-2 max-sm:col-span-1 max-sm:col-start-1 p-4 flex items-center justify-center border backdrop-blur-sm border-yellow-400">
-          <Image
-            src="/images/partners/universe.png"
-            height={350}
-            width={350}
-            alt="Universe23"
-          />
+          <Image src={universe} height={350} width={350} alt="Universe23" />
         </div>
         <div className="col-start-7 col-span-2 p-4 max-sm:col-span-1 max-sm:col-start-1 flex items-center justify-center border backdrop-blur-sm border-yellow-400 rounded-br-lg md:rounded-br-3xl  max-sm:rounded-bl-lg">
-          <Image
-            src="/images/partners/universe.png"
-            height={350}
-            width={350}
-            alt="Universe23"
-          />
+          <Image src={universe} height={350} width={350} alt="Universe23" />
         </div>
       </div>
     </section>
