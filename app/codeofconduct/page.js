6a94c621d482ee1520d0dde591281168e9293b01
shahzaib-ai <shahzaib1234@outlook.com>
2023-11-04 21:37:30 +0500

    Improved navlink paths

--- app/codeofconduct/page.js
+++ app/codeofconduct/page.js
@@ -1,9 +1,13 @@
 import CodeofConduct from "./COC";
+import Nav from "@/components/Nav";
+import Footer from "@/components/Footer";
 
 export default function CodeConduct() {
   return (
     <main className="relative">
+      <Nav />
       <CodeofConduct />
+      <Footer />
     </main>
   );
 }
--- constants/index.js
+++ constants/index.js
@@ -29,13 +29,13 @@ export const teamMembers = [
 ];
 
 export const navLinks = [
-  { href: "#about", label: "About" },
-  { href: "#schedule", label: "Event Schedule" },
-  { href: "#team", label: "Team" },
-  { href: "#partners", label: "Partners" },
-  { href: "#sponsers", label: "Sponsers" },
-  { href: "#highlights", label: "Last Year Highlights" },
-  { href: "#faq", label: "FAQs" },
+  { href: "/#about", label: "About" },
+  { href: "/#schedule", label: "Event Schedule" },
+  { href: "/#team", label: "Team" },
+  { href: "/#partners", label: "Partners" },
+  { href: "/#sponsers", label: "Sponsers" },
+  { href: "/#highlights", label: "Last Year Highlights" },
+  { href: "/#faq", label: "FAQs" },
   { href: "/codeofconduct", label: "Code of conduct" },
 ];
 
