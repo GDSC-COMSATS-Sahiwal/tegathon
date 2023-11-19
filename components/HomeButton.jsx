2ea7ffdbd874a09e66cd5215ce7e30e2b181de5c
shahzaib-ai <shahzaib1234@outlook.com>
2023-11-19 10:07:42 +0500

    Reimplemented the mobile navbar

--- components/HomeButton.jsx
+++ components/HomeButton.jsx
@@ -1,13 +1,13 @@
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
 
 const HomeButton = () => {
   return (
     <>
-      <Link href="/">
-        <Button className="bg-[#FDD200] text-[#333333] text-base font-bold hover:bg-yellow-500 hover:text-white px-12 py-6 rounded-2xl">
-          Home
-        </Button>
+      <Link
+        className=" text-[#FDD200] active:text-white text-base font-bold  px-4 py-4 rounded-2xl"
+        href="/"
+      >
+        Home
       </Link>
     </>
   );
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,14 +4,14 @@ import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <div className="space-y-4 py-4 flex flex-col h-screen text-[#fffffe] glass-container">
+    <div className="space-y-4 py-12 flex flex-col h-screen text-[#fffffe] glass-container border-0">
       <div className="px-3 py-2 flex-1">
-        <div className="space-y-1">
+        <div className="space-y-1 mt-8">
           {navLinks.map((navLink) => (
             <Link
               href={navLink.href}
               key={navLink.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cusror-pointer hover:text-[#0f0e17] hover:bg-[#0f0e17]/10 rounded-lg transition"
+              className="text-sm group flex p-3 w-full justify-start font-medium cusror-pointer text-[#FDD200] hover:text-white hover:bg-white/25 rounded-lg transition"
             >
               <div className="flex items-center flex-1">{navLink.label}</div>
             </Link>
