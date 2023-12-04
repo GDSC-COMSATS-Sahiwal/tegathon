b2751497431dc19f5074c1d721cf7b704c303647
shahzaib-ai <shahzaib1234@outlook.com>
2023-12-04 07:11:24 +0500

    fixed layout breaking issue for higher ppi devices

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -25,6 +25,7 @@ const Hero = () => {
                 alt="tegathon logo"
                 width={800}
                 height={800}
+                className="relative z-[2]"
               />
             </div>
             <div className="md:space-y-12 space-y-4 md:px-10 ">
@@ -35,13 +36,13 @@ const Hero = () => {
               </p>
               <div className="flex gap-6 items-center w-full justify-center md:justify-start">
                 <Link href={'https://linktr.ee/tegathon2.0'} target="blank">
-                  <button className="bg-[#FDD200] text-[#333333] text-base md:p-5 sm:px-2 px-4 py-4 font-bold sm:rounded-3xl rounded-2xl">
+                  <button className="bg-[#FDD200] text-[#333333] text-base md:p-5 sm:px-2 px-4 py-4 font-bold sm:rounded-3xl rounded-2xl whitespace-nowrap relative z-[2]">
                     Register Now
                   </button>
                 </Link>
                 <Link
                   href="/codeofconduct"
-                  className="border border-[#FDD200] text-[#FDD200] text-base md:p-5 sm:px-2 px-4 py-4 font-bold sm:rounded-3xl rounded-2xl"
+                  className="border border-[#FDD200] text-[#FDD200] text-base md:p-5 sm:px-2 px-4 py-4 font-bold sm:rounded-3xl rounded-2xl bg-black/50 backdrop-blur-2xl whitespace-nowrap relative z-[2]"
                 >
                   Code of Conduct
                 </Link>
@@ -56,7 +57,7 @@ const Hero = () => {
               alt="lines"
               width={700}
               height={700}
-              className="absolute right-0 bottom-0"
+              className="absolute right-0 bottom-0 max-w-lg md:max-w-[600px]"
             />
             <Image
               priority={true}
@@ -64,7 +65,7 @@ const Hero = () => {
               alt="astro"
               width={600}
               height={600}
-              className="absolute right-0 bottom-0"
+              className="absolute right-0 mx-auto sm:mr-auto bottom-0 max-w-sm md:max-w-[500px]"
             />
             <div className="glass-container py-2 px-6 rounded-3xl text-center relative md:-mb-24 md:-mr-10 text-[#FDD200] font-bold">
               <EventTimer eventDate={eventDate} />
@@ -74,7 +75,7 @@ const Hero = () => {
       </div>
       {/* news line animation  */}
       <div className="absolute bottom-0 right-0 py-3 md:py-4 w-full overflow-hidden bg-gradient-to-r from-yellow-500 via-yellow-600 to-red-600">
-        <p className="text-lg md:text-xl  text-black font-normal scrolling-text whitespace-nowrap">
+        <p className="text-lg md:text-xl  text-black font-normal scrolling-text whitespace-nowrap relative z-[2]">
           Tegathon is a unique, three-day event that brings together the worlds
           of technology, gaming, and coding in an exhilarating fusion of
           innovation and fun. We've coined the term "Tegathon" by combining "Te"
