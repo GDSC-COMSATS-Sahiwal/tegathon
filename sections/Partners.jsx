df70b8a4927b66627ca4dfeab94fefcb24d2a3e8
Ahsan Raza <ahsanraza00059@gmail.com>
2024-05-11 23:33:04 +0500

    Update Partners.jsx

--- sections/Partners.jsx
+++ sections/Partners.jsx
@@ -5,7 +5,7 @@ const Partners = () => {
       <h1 className="section-title">Partners</h1>
 
       <div className="grid grid-cols-8 max-sm:grid-cols-6 w-full glass-container rounded-lg md:rounded-3xl border border-[#FDD200]">
-        <div className="col-start-1 col-span-2 p-4 max-sm:col-span-3 max-sm:col-start-4 flex items-center justify-center rounded-tl-lg md:rounded-tl-3xl border-[#FDD200] max-sm:border-r max-sm:rounded-tl-lg">
+        <div className="col-start-1 col-span-2 p-4 max-sm:col-span-2 max-sm:col-start-1 flex items-center justify-center rounded-tl-lg md:rounded-tl-3xl border-[#FDD200] max-sm:border-r max-sm:rounded-tl-lg">
           <Image
             src="/sections/partner/github.webp"
             height={350}
@@ -14,7 +14,7 @@ const Partners = () => {
             alt="Github"
           />
         </div>
-        <div className="col-start-3 col-span-2 p-4 max-sm:col-span-3 max-sm:col-start-4 flex items-center justify-center sm:border-l border-t max-sm:border-t-0 border-yellow-400">
+        <div className="col-start-3 col-span-2 p-4 max-sm:col-span-2 max-sm:col-start-3 flex items-center justify-center sm:border-l border-t max-sm:border-t-0 border-yellow-400">
           <Image
             src="/sections/partner/gdsc.webp"
             height={350}
@@ -23,7 +23,7 @@ const Partners = () => {
             alt="Github"
           />
         </div>
-        <div className="col-start-5 col-span-2 p-4 max-sm:col-span-3 max-sm:col-start-4 flex items-center justify-center sm:border-l border-t max-sm:border-t-0 border-yellow-400">
+        <div className="col-start-5 col-span-2 p-4 max-sm:col-span-2 max-sm:col-start-5 flex items-center justify-center border-l border-t max-sm:border-t-0 border-yellow-400">
           <Image
             src="/sections/partner/mlsa.webp"
             height={350}
